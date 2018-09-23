9d51e9c3eb1af3a33c5f26daee6fd895c16a76ee
Ilya <mra4niykeks@gmail.com>
2018-09-23 15:44:00 +0300

    refactoring. beat the ball added. restart the game after fall

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,13 @@
 #define INITIAL_X_PLAYER_1 SSD1306_LCDWIDTH / 32
 #define BALL_RADIUS 2
 
-int y0, y1;
 double ball_x = SSD1306_LCDWIDTH / 2;
 double ball_y = SSD1306_LCDHEIGHT / 2;
-
 int BALL_SPEED_X = 2;
 int BALL_SPEED_Y = 2;
+
+int player_position_y0 = INITIAL_Y_PLAYER;
+int player_position_y1 = player_position_y0 + PLAYER_LENGTH;
 
 
 Adafruit_SSD1306 display(OLED_RESET);
@@ -43,9 +44,52 @@ void setup() {
   display.clearDisplay();
 }
 
+void reset_game_field() {
+  display.clearDisplay();
+  display.drawLine(INITIAL_X_PLAYER_1,
+    INITIAL_Y_PLAYER,
+    INITIAL_X_PLAYER_1,
+    INITIAL_Y_PLAYER + PLAYER_LENGTH,
+    WHITE
+  );
+  ball_x = SSD1306_LCDWIDTH / 2;
+  ball_y = SSD1306_LCDHEIGHT / 2;
+
+  player_position_y0 = INITIAL_Y_PLAYER;
+  player_position_y1 = player_position_y0 + PLAYER_LENGTH;
+  
+  display.fillCircle(ball_x, ball_y, BALL_RADIUS, WHITE);
+  BALL_SPEED_X = 2;
+  BALL_SPEED_Y = 2;
+  display.display();
+}
+
 void ball_movement(double *ball_x, double *ball_y) {
-  if (*ball_x == SSD1306_LCDWIDTH - BALL_RADIUS *2 ) {
+  if (*ball_x == SSD1306_LCDWIDTH - BALL_RADIUS *2 ||
+      (*ball_x == INITIAL_X_PLAYER_1 + BALL_RADIUS *2
+        && *ball_y >= player_position_y0 &&  *ball_y <= player_position_y1)) {
     BALL_SPEED_X = BALL_SPEED_X * -1;
+  } else if (*ball_x < INITIAL_X_PLAYER_1){
+      BALL_SPEED_X = 0;
+      BALL_SPEED_Y = 0;
+
+      delay(3000);
+
+      display.invertDisplay(true);
+      delay(300);
+      display.invertDisplay(false);
+      delay(300);
+      display.invertDisplay(true);
+      delay(300);
+      display.invertDisplay(false);
+      delay(300);
+      display.invertDisplay(true);
+      delay(300);
+      display.invertDisplay(false);
+      delay(300);
+      display.clearDisplay();
+      reset_game_field();
+      // return;
   }
 
   if (*ball_y == SSD1306_LCDHEIGHT - BALL_RADIUS *2 || *ball_y == BALL_RADIUS *2) {
@@ -67,22 +111,22 @@ void ball_movement(double *ball_x, double *ball_y) {
 }
 
 
-
 void loop() {
     display.clearDisplay();
     int y = analogRead(Y_PIN);
 
     y = round((AXIS_Y_MID - y) / 100);
-    if ((y0 + y + INITIAL_Y_PLAYER) > 0 && (y1 + INITIAL_Y_PLAYER + PLAYER_LENGTH + y) < SSD1306_LCDHEIGHT) {
-      y0 += y;
-      y1 += y;
+
+    if (player_position_y0 + y > 0 && (player_position_y1 + y) < SSD1306_LCDHEIGHT) {
+      player_position_y0 += y;
+      player_position_y1 += y;
     }
 
-    if (INITIAL_Y_PLAYER + y0 >= 0 && INITIAL_Y_PLAYER + PLAYER_LENGTH + y1 <= SSD1306_LCDHEIGHT) {
+    if (player_position_y0 >= 0 && player_position_y1 <= SSD1306_LCDHEIGHT) {
       display.drawLine(INITIAL_X_PLAYER_1,
-        INITIAL_Y_PLAYER + y0,
+        player_position_y0,
         INITIAL_X_PLAYER_1,
-        INITIAL_Y_PLAYER + PLAYER_LENGTH + y1,
+        player_position_y1,
         WHITE
       );
     }
@@ -93,5 +137,5 @@ void loop() {
     // Since the buffer is intialized with an Adafruit splashscreen
     // internally, this will display the splashscreen.
     display.display();
-    delay(200);
+    delay(1);
 }
