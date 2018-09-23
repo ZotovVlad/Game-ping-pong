f7bc0614ded4819621103859108119e381e3fc9d
Ilya <mra4niykeks@gmail.com>
2018-09-23 16:36:23 +0300

    playground is blue field only

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,27 @@
 #define X_PIN PC1
 #define Y_PIN PC0
 #define BUTTON PB1
+
 #define AXIS_Y_MIN 0
 #define AXIS_Y_MID 350
 #define AXIS_Y_MAX 700
 
-#define PLAYER_LENGTH SSD1306_LCDHEIGHT / 4
-#define INITIAL_Y_PLAYER SSD1306_LCDHEIGHT / 4
+#define BLUE_FIELD_FIRST_PIXEL 16
+#define BLUE_FIELD_WIDTH 128
+#define BLUE_FIELD_HEIGHT 48
+
+#define BORDER_LINE_WIDTH 1
+#define BORDER_WIDTH BLUE_FIELD_WIDTH
+#define BORDER_HEIGHT BLUE_FIELD_HEIGHT
+
+#define PLAYER_LENGTH BLUE_FIELD_HEIGHT / 4
+#define INITIAL_Y_PLAYER BLUE_FIELD_HEIGHT / 4 + BLUE_FIELD_FIRST_PIXEL
 #define INITIAL_X_PLAYER_1 SSD1306_LCDWIDTH / 32
 #define BALL_RADIUS 2
 
 double ball_x = SSD1306_LCDWIDTH / 2;
-double ball_y = SSD1306_LCDHEIGHT / 2;
+double ball_y = (SSD1306_LCDHEIGHT / 2) + BLUE_FIELD_FIRST_PIXEL;
+
 int BALL_SPEED_X = 2;
 int BALL_SPEED_Y = 2;
 
@@ -53,21 +63,24 @@ void reset_game_field() {
     WHITE
   );
   ball_x = SSD1306_LCDWIDTH / 2;
-  ball_y = SSD1306_LCDHEIGHT / 2;
+  ball_y = (SSD1306_LCDHEIGHT / 2) + BLUE_FIELD_FIRST_PIXEL;
 
   player_position_y0 = INITIAL_Y_PLAYER;
   player_position_y1 = player_position_y0 + PLAYER_LENGTH;
-  
+
   display.fillCircle(ball_x, ball_y, BALL_RADIUS, WHITE);
   BALL_SPEED_X = 2;
   BALL_SPEED_Y = 2;
+
+  display.drawRect(0, BLUE_FIELD_FIRST_PIXEL, BORDER_WIDTH, BORDER_HEIGHT, WHITE);
+
   display.display();
 }
 
 void ball_movement(double *ball_x, double *ball_y) {
   if (*ball_x == SSD1306_LCDWIDTH - BALL_RADIUS *2 ||
       (*ball_x == INITIAL_X_PLAYER_1 + BALL_RADIUS *2
-        && *ball_y >= player_position_y0 &&  *ball_y <= player_position_y1)) {
+        && *ball_y >= player_position_y0 && *ball_y <= player_position_y1)) {
     BALL_SPEED_X = BALL_SPEED_X * -1;
   } else if (*ball_x < INITIAL_X_PLAYER_1){
       BALL_SPEED_X = 0;
@@ -92,7 +105,7 @@ void ball_movement(double *ball_x, double *ball_y) {
       // return;
   }
 
-  if (*ball_y == SSD1306_LCDHEIGHT - BALL_RADIUS *2 || *ball_y == BALL_RADIUS *2) {
+  if (*ball_y == SSD1306_LCDHEIGHT - BALL_RADIUS *2 || *ball_y == BLUE_FIELD_FIRST_PIXEL + BALL_RADIUS *2) {
     BALL_SPEED_Y = BALL_SPEED_Y * -1;
   }
 
@@ -105,24 +118,25 @@ void ball_movement(double *ball_x, double *ball_y) {
   if (*ball_y > SSD1306_LCDHEIGHT) {
     *ball_y = SSD1306_LCDHEIGHT - BALL_RADIUS *2;
   }
-  if(*ball_y < BALL_RADIUS *2){
-    *ball_y = BALL_RADIUS *2;
+  if(*ball_y < BLUE_FIELD_FIRST_PIXEL + BALL_RADIUS *2){
+    *ball_y = BLUE_FIELD_FIRST_PIXEL + BALL_RADIUS *2;
   }
 }
 
 
 void loop() {
     display.clearDisplay();
+    display.drawRect(0, BLUE_FIELD_FIRST_PIXEL, BORDER_WIDTH, BORDER_HEIGHT, WHITE);
     int y = analogRead(Y_PIN);
 
     y = round((AXIS_Y_MID - y) / 100);
 
-    if (player_position_y0 + y > 0 && (player_position_y1 + y) < SSD1306_LCDHEIGHT) {
+    if (player_position_y0 + y > BLUE_FIELD_FIRST_PIXEL && (player_position_y1 + y) < SSD1306_LCDHEIGHT) {
       player_position_y0 += y;
       player_position_y1 += y;
     }
 
-    if (player_position_y0 >= 0 && player_position_y1 <= SSD1306_LCDHEIGHT) {
+    if (player_position_y0 > BLUE_FIELD_FIRST_PIXEL && player_position_y1 < SSD1306_LCDHEIGHT) {
       display.drawLine(INITIAL_X_PLAYER_1,
         player_position_y0,
         INITIAL_X_PLAYER_1,
